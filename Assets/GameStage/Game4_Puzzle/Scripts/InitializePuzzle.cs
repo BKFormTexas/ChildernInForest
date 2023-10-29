5726080a9e69fdddd46c1a6e5b274eb307853d6b
ByungKwon Lee <88296556+byungkwon2023F@users.noreply.github.com>
2023-10-29 17:54:31 -0400

    Update InitializePuzzle.cs

--- Assets/GameStage/Game4_Puzzle/Scripts/InitializePuzzle.cs
+++ Assets/GameStage/Game4_Puzzle/Scripts/InitializePuzzle.cs
@@ -1,28 +1,28 @@
 /*
- * - Name : InitializePuzzle.cs
- * - Writer : 최대준
- * - Content : Text to Speech Class로, 디자인 패턴은 싱글톤 패턴을 이용하였다. 조금 무거운 클래스 이므로 하나의 인스턴스를 다른 오브젝트 클래스에서 재사용할 수 있도록 하기 위해서 싱글톤 패턴을 이용하였다.
+ * - Name: InitializePuzzle.cs
+ * - Content: This is a class for Text-to-Speech, utilizing the Singleton pattern. Since it's a somewhat heavy class, the Singleton pattern is used to allow reuse of a single instance in other object classes.
  * - History -
- * 2021-07-28 : 첫 구현.
- * 2021-07-29 : 주석 처리.
+ * 2021-07-28: First implementation.
+ * 2021-07-29: Commenting.
  *
- * - InitializePuzzle Member Variable 
+ * - InitializePuzzle Member Variables
  *
- * mf_originWidth, mf_originHeight : 퍼즐 윈도우 즉 퍼즐이 존재하는 창의 너비, 높이를 나타낸다.
- * mrt_parentSize : 퍼즐 윈도우 크기를 RectTransform클래스 형태로 표기한다.
- * mgo_SetPuzzle : 퍼즐의 프리팹을 담는 변수이다.
- * mglg_AnsGridSize : 퍼즐은 그리드 형태로 윈도우 창에 존재하게 되므로 그때 퍼즐의 크기를 조절한다.
- * mglg_ProbGridSize : 퍼즐을 그리드 형태로 있게 해주는 컴포넌트이다.
- * mspl_slicedPuzzle : SpriteSlice.sliceSprite() 함수는 반환값이 Sprite[] 이므로 반환값을 저장하는 변수이다.
- * mtex2_slicePuzzle : 짜를 이미지를 인스펙터 창에서 담아온다.
- 
- * - InitializePuzzle Member Function
+ * mf_originWidth, mf_originHeight: Represent the width and height of the puzzle window, where the puzzles exist.
+ * mrt_parentSize: Represents the puzzle window size as a RectTransform.
+ * mgo_SetPuzzle: Stores the puzzle prefab.
+ * mglg_AnsGridSize: Adjusts the size of puzzles in a grid since puzzles exist in a grid inside the window.
+ * mglg_ProbGridSize: A component that allows puzzles to exist in a grid format.
+ * mspl_slicedPuzzle: Since the SpriteSlice.sliceSprite() function returns a Sprite[], this variable stores the result.
+ * mtex2_slicePuzzle: Stores the image to be sliced, which is set in the Inspector window.
+ * myArray: An array for randomizing the order of puzzles.
  *
- * Awake() : 씬이 시작될 때, 퍼즐 윈도우 사이즈에 따라 그리드 세팅 값과 그 안에 존재하는 퍼즐들을 생성하는 함수이다.  
- * setChild() : 퍼즐 윈도우 안에 퍼즐 게임오브젝트를 생성한다. 이때 퍼즐 윈도우의 자식 오브젝트로 들어가게 만든다.
- * 섭종
+ * - InitializePuzzle Member Functions
+ *
+ * Awake(): Adjusts grid cell sizes and generates puzzle pieces within the puzzle window based on the window size when the scene starts.
+ * setChild(): Generates puzzle game objects inside the puzzle window. It ensures they become child objects of the puzzle window.
+ * Author: Choi Daejun
  */
-
+ 
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
@@ -30,7 +30,7 @@ using UnityEngine;
 using UnityEngine.UI;
  
 /// <summary>
-/// 본 클래스는 퍼즐 스테이지의 초기화를 담당한다. 퍼즐 조각을 씬에 생성하게 된다.
+/// This class is responsible for initializing the puzzle stage. Puzzle pieces are created in the scene.
 /// </summary>
 public class InitializePuzzle : MonoBehaviour {
     private float mf_originWidth, mf_originHeight;
@@ -41,9 +41,9 @@ public class InitializePuzzle : MonoBehaviour {
     private Sprite[] mspl_slicedPuzzle;
     public Texture2D mtex2_slicePuzzle;
     private int[] myArray;
-    /// <summary>
-    /// 윈도우 사이즈에 맞춰 그리드 셀 사이즈를 조절하고, 그 그리드에 맞춰 퍼즐 조각을 생성해준다.
-    /// </summary>
+     /// <summary>
+     /// Adjusts the grid cell size according to the window size and creates puzzle pieces according to the grid.
+     /// </summary>
     void Awake() {
         int col = 0;
         int row = 0;
@@ -85,13 +85,12 @@ public class InitializePuzzle : MonoBehaviour {
         setChild(col * row, transform.GetChild(2), false);
     }
 
-    /// <summary>
-    /// 퍼즐 윈도우 오브젝트의 자식 오브젝트로써 퍼즐 조각을 생성하는 함수이다.
-    /// </summary>
-    /// <param name="nChild">해당 tParent 오브젝트 아래로 몇개의 자식 오브젝트를 생성할 지를 지정해주면 된다.</param>
-    /// <param name="tParent">자식 오브젝트를 생성할 부모 오브젝트를 지정해주면 된다.</param>
-    /// <param name="bClassifyType">정답 퍼즐인지, 플레이어가 조종하는 퍼즐인지에 맞춰 오브젝트 세팅을 변경한다.</param>
-    void setChild(int nChild, Transform tParent, bool bClassifyType) {
+   /// <summary>
+     /// This is a function that creates a puzzle piece as a child object of the puzzle window object.
+     /// </summary>
+     /// <param name="nChild">Just specify how many child objects will be created under the tParent object.</param>
+     /// <param name="tParent">Just specify the parent object in which to create the child object.</param>
+     /// <param name="bClassifyType">Change object settings depending on whether it is a correct answer puzzle or a player-controlled puzzle.</param>   void setChild(int nChild, Transform tParent, bool bClassifyType) {
         Sprite[] tempSprite = new Sprite[mspl_slicedPuzzle.Length];
         Debug.Log(tempSprite[0]);
         for (int i = 0; i < nChild; i++) {
