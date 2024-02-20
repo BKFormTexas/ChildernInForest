631b20fcb854aabcf1461b9b8383cba4a660e017
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:22:24 -0500

    Update BlinkAni.cs

--- Assets/FairytaleStage/Jack/Jack_Epi13/Scripts/BlinkAni.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi13/Scripts/BlinkAni.cs
@@ -1,33 +1,37 @@
 /*
- * - Name : BlinkAni.cs
- * - Writer : 이윤교
- * - Content : 잭과콩나무 에피소드13 - 화살표 깜빡이는 효과 스크립트
+ * - Name: BlinkAni.cs
+ * - Content: Jack and the Beanstalk Episode 13 - Script for Blinking Arrow Effect
  * 
  * - HISTORY
- * 2021-07-14 : 초기 개발
- * 2021-07-16 : 파일 인코딩 수정 및 주석 처리
- * 2021-07-27 : 주석 처리 수정
+ * 2021-07-14: Initial development
+ * 2021-07-16: File encoding modification and comment processing
+ * 2021-07-27: Comment processing modification
  *
  * <Variable>
- * f_blink : 깜빡거리는 속도          
+ * f_blink: Blinking speed          
  */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 미션 화살표 깜빡이게 하는 애니메이션 클래스
-public class BlinkAni : MonoBehaviour{
-    float f_blink; 
+// Animation class for making the mission arrow blink
+public class BlinkAni : MonoBehaviour
+{
+    float f_blink;
 
-    /*깜빡깜빡 거리는 효과*/
-    public void Update(){
-        if (f_blink < 0.5f){
-            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1); //투명도 1
+    /*Blinking effect*/
+    public void Update()
+    {
+        if (f_blink < 0.5f)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1); // Full opacity
         }
-        else{
-            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0); //투명도 0
-            if (f_blink > 1f){
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0); // Fully transparent
+            if (f_blink > 1f)
+            {
                 f_blink = 0;
             }
         }
