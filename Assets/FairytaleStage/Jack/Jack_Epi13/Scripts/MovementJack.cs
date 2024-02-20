4b8ab6fe123b3df209c68b2fce2000eb72ac28c6
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:24:50 -0500

    Update MovementJack.cs

--- Assets/FairytaleStage/Jack/Jack_Epi13/Scripts/MovementJack.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi13/Scripts/MovementJack.cs
@@ -1,64 +1,63 @@
 /*
- * - Name : Movement_Jack.cs
- * - Writer : 이윤교
- * - Content : 잭과콩나무 에피소드13 - 잭 이동 스크립트
- * 
- * - HISTORY
- * 2021-07-15 : 초기 개발
- * 2021-07-16 : 파일 인코딩 수정 및 주석 처리
- * 2021-07-22 : TTS 기능 추가
- * 2021-07-26 : 효과음 추가 완료
- * 2021-07-27 : 주석 처리 수정
- *
- * <Variable>
- * v3_target : 작이 걸어가야하는 위치 지정
- * mb_checkPlayOnce : 스크립트 음성이 한번만 실행되게 하기 위한 체크하는 변수
- * mb_checkPlayVoice : 첫번째 스크립트 재생이 되었는지 체크하는 변수
- * sc : 동화 스크립트 나타내기 위한 오브젝트
- * vm : 음성 TTS를 처리하는 오브젝트 연결 
- * ScreamSound : 잭 비명소리
- *
- * <Function>
- * PlayScream() : 잭 비명소리 재생하는 함수
- */
+  * - Name: Movement_Jack.cs
+  * - Content: Jack and the Beanstalk Episode 13 - Jack Movement Script
+  *
+  * - HISTORY
+  * 2021-07-15: Initial development
+  * 2021-07-16: Fixed file encoding and commented out
+  * 2021-07-22: TTS function added
+  * 2021-07-26: Sound effect added completed
+  * 2021-07-27: Fix comment processing
+  *
+  * <Variable>
+  * v3_target: Specifies the location where the object should walk
+  * mb_checkPlayOnce: Variable to check to ensure that the script voice is executed only once
+  * mb_checkPlayVoice: Variable that checks whether the first script has been played.
+  * sc: Object to represent fairy tale script
+  * vm: Object connection that handles voice TTS
+  * ScreamSound: Jack's scream
+  *
+  * <Function>
+  * PlayScream(): Function to play Jack's scream
+  */
 
-using System.Collections;
+using System. Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//잭 이동 클래스
-public class MovementJack : MonoBehaviour{
-    public Vector3 v3_target;
-    bool mb_checkPlayOnce = false;
-    bool mb_checkPlayVoice = false;
-    public ScriptControl sc;
-    VoiceManager vm;
-    private AudioSource ScreamSound;
+//Jack movement class
+public class MovementJack: MonoBehaviour{
+     public Vector3 v3_target;
+     bool mb_checkPlayOnce = false;
+     bool mb_checkPlayVoice = false;
+     public ScriptControl sc;
+     VoiceManager vm;
+     private AudioSource ScreamSound;
 
-    //초기설정
-    void Start(){
-        sc = ScriptControl.GetInstance();
-        this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
-        ScreamSound = GameObject.Find("ScreamSound").GetComponent<AudioSource>();
-    Invoke("PlayScream",1f);                                                                //1초 후 PlayScream 함수 재생
-    }
-    void Update(){
-        if(vm.mb_checkSceneReady) {                                                         //tts 준비 작업이 다 끝났다면 
-            transform.position = Vector3.MoveTowards(transform.position, v3_target, 0.2f);  // 잭 이동
-            if(!mb_checkPlayOnce) {                                                         //스크립트 음성이 한번도 나온적이 없다면
-                vm.playVoice(0);                                                            //스크립트 음성 재생   
-                mb_checkPlayOnce = true;                                                    //스크립트 음성 재생 체크
-            }
-            if(!vm.isPlaying() && !mb_checkPlayVoice){                                      //현재 스크립트 음성이 끝나고 다음 스크립트 음성이 재생된 적이 없다면
-                sc.setNextScript();                                                         //다음 스크립트 보여줌
-                vm.playVoice(1);                                                            //다음 스크립트 음성 재생   
-                mb_checkPlayVoice = true;                                                   //스크립트 음성 재생 체크
-            }
-        }
-    }
+     //Initial settings
+     void Start(){
+         sc = ScriptControl.GetInstance();
+         this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
+         ScreamSound = GameObject.Find("ScreamSound").GetComponent<AudioSource>();
+     Invoke("PlayScream",1f); //Play PlayScream function after 1 second
+     }
+     void Update(){
+         if(vm.mb_checkSceneReady) { //If tts preparation work is completed
+             transform.position = Vector3.MoveTowards(transform.position, v3_target, 0.2f); // move jack
+             if(!mb_checkPlayOnce) { //If the script voice has never been played
+                 vm.playVoice(0); //Play script voice
+                 mb_checkPlayOnce = true; //Check script voice playback
+             }
+             if(!vm.isPlaying() && !mb_checkPlayVoice){ //If the current script voice has ended and the next script voice has never been played
+                 sc.setNextScript(); //Show the next script
+                 vm.playVoice(1); //Play next script voice
+                 mb_checkPlayVoice = true; //Check script voice playback
+             }
+         }
+     }
 
-    // 잭 비명소리 재생하는 함수
-    void PlayScream(){
-        ScreamSound.Play();
-    }
+     // Function to play Jack's scream sound
+     void PlayScream(){
+         ScreamSound.Play();
+     }
 }
