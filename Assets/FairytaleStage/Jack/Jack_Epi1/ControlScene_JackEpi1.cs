0cd9f85668fc8777e88aaeedc04693ab36a9b89e
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:19:34 -0500

    Update ControlScene_JackEpi1.cs

--- Assets/FairytaleStage/Jack/Jack_Epi1/ControlScene_JackEpi1.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi1/ControlScene_JackEpi1.cs
@@ -1,26 +1,25 @@
-﻿/*
- * - Name : ControlScene_JackEpi1.cs
- * - Writer : 최대준
- * - Content : 잭과콩나무 에피소드1 - 
+/*
+ * - Name: ControlScene_JackEpi1.cs
+ * - Content: Jack and the Beanstalk Episode 1 -
  *
  * - History -
- * 2021-07-20 : 주석 작성.
- * 2021-07-23 : TTS 음성 출력 기능 추가.
- * 2021-07-27 : 피드백에 의한 주석 변경.
+ * 2021-07-20: Comment added.
+ * 2021-07-23: Added TTS (Text-to-Speech) voice output functionality.
+ * 2021-07-27: Comment modifications based on feedback.
  *
- * - ControlScene_JackEpi1 Member variable
- * 
- * ms_LoadScene : 다음 씬을 인스펙터창에서 입력하도록 하여 저장하는 변수이다.
- * mb_PlayOnce = false : 음성이 한번만 출력하도록 체크하는 변수이다.
- * mvm_PlayVoice : 음성을 준비하고 출력하는 클래스이다.
- * 
- * - ControlScene_JackEpi1 Member function
+ * - ControlScene_JackEpi1 Member Variables
  *
- * GetMouseButtonUp(0) : 좌클릭시 clickedMouse 함수를 호출하는 함수.
- * SceneManager.LoadScene("") : 다음 Scene으로 이동 하는 함수.
- * Start() : 음성을 출력하기 위해서 VoiceManager를 초기화한다.
- * Update() : 음성이 준비가 되었다면 한번만 출력하도록 한다.
- * 
+ * ms_LoadScene: Variable to store the next scene by allowing input in the Inspector window.
+ * mb_PlayOnce = false: Variable to check if the voice should be played only once.
+ * mvm_PlayVoice: Class for preparing and outputting voice.
+ *
+ * - ControlScene_JackEpi1 Member Functions
+ *
+ * GetMouseButtonUp(0): Function that calls the clickedMouse function on left-click.
+ * SceneManager.LoadScene(""): Function to move to the next scene.
+ * Start(): Initializes VoiceManager to output voice.
+ * Update(): Ensures that the voice is played only once when ready.
+ *
  */
 
 using System.Collections;
@@ -28,7 +27,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// 잭과 콩나무 첫번째 씬에서 다음 씬으로 넘어가는 클래스이다.
+// Class for transitioning from the first scene of Jack and the Beanstalk to the next scene.
 public class ControlScene_JackEpi1 : MonoBehaviour {
     public string ms_LoadScene;
     public bool mb_PlayOnce = false;
@@ -38,7 +37,7 @@ public class ControlScene_JackEpi1 : MonoBehaviour {
         mvm_PlayVoice = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
     }
 
-    // 씬에서 플레이어가 좌클릭하는 것을 감지하면, clickedMouse 함수를 호출한다.
+    // Detects left-click by the player in the scene and calls the clickedMouse function.
     void Update() {
         if(mvm_PlayVoice.mb_checkSceneReady && !mb_PlayOnce) {
             mvm_PlayVoice.playVoice(0);
@@ -52,9 +51,8 @@ public class ControlScene_JackEpi1 : MonoBehaviour {
 
     }
 
-    // 이 함수가 호출되면 다음 씬으로 넘어가도록 하였다.
+    // When this function is called, it transitions to the next scene.
     void clickedMouse() {
         SceneManager.LoadScene(ms_LoadScene);
     }
-    
 }
