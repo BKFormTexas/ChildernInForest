b0df4852824506c661b83c303ad459b8018bbb25
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:28:55 -0500

    Update DragCharacters.cs

--- Assets/FairytaleStage/Jack/Jack_Epi2/Scripts/DragCharacters.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi2/Scripts/DragCharacters.cs
@@ -1,35 +1,35 @@
-﻿/*
- * - Name : DragCharacters.cs
- * - Writer : 류시온
- * - Content : 잭과콩나무 에피소드2 - 잭,소 드래그 스크립트
- * - History -
- * 2021-07-20 : 작성
- * 2021-07-27 : 피드백에 의한 주석 변경.
- *
- * DragCharacters Member Variables
- * 
- * null
- * 
- * DragCharacters Member Functions
- *
- * OnTriggerEnter2D() :오브젝트간 충돌이 일어날때 처음 한번만 호출되는 함수
- * OnMouseDrag() : 게임오브젝트를 마우스 드래그로 이동시키는 함수
- * 
- */
+/*
+  * - Name: DragCharacters.cs
+  * - Writer: Ryu Shi-on
+  * - Content: Jack and the Beanstalk Episode 2 - Jack and Cow drag script
+  * - History -
+  * 2021-07-20: Created
+  * 2021-07-27: Annotation changed based on feedback.
+  *
+  * DragCharacters Member Variables
+  *
+  *null
+  *
+  * DragCharacters Member Functions
+  *
+  * OnTriggerEnter2D(): A function that is called only once when a collision occurs between objects.
+  * OnMouseDrag(): Function to move a game object by dragging the mouse
+  *
+  */
 
-using System.Collections;
+using System. Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 씬에 등장하는 캐릭터를 플레이어가 클릭하여 드래그하는 것을 감지하는 클래스이다.
-public class DragCharacters : MonoBehaviour
+// This is a class that detects when the player clicks and drags a character that appears in the scene.
+public class DragCharacters: MonoBehaviour
 {
-    // 드래그할시에 캐릭터를 마우스 위치로 옮기도록 하였다.
-    private void OnMouseDrag()
-    {
-        Vector2 v2_checkMousePos = new Vector2(Input.mousePosition.x,
-        Input.mousePosition.y);
-        Vector2 v2_checkworldObjPos = Camera.main.ScreenToWorldPoint(v2_checkMousePos);
-        this.transform.position = v2_checkworldObjPos;
-    }
+     // When dragging, the character is moved to the mouse position.
+     private void OnMouseDrag()
+     {
+         Vector2 v2_checkMousePos = new Vector2(Input.mousePosition.x,
+         Input.mousePosition.y);
+         Vector2 v2_checkworldObjPos = Camera.main.ScreenToWorldPoint(v2_checkMousePos);
+         this.transform.position = v2_checkworldObjPos;
+     }
 }
