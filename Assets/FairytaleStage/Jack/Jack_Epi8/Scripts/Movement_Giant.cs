ceb24b17b199b146f3d805ada30ec7e081b506b6
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:36:44 -0500

    Update Movement_Giant.cs

--- Assets/FairytaleStage/Jack/Jack_Epi8/Scripts/Movement_Giant.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi8/Scripts/Movement_Giant.cs
@@ -1,58 +1,57 @@
 /*
-  * - Name : Movement_Giant.cs
-  * - Writer : 이윤교
-  * - Content : 잭과콩나무 에피소드8 - 잭 뒤를 쫓도록 거인이 움직이는 클래스/
-  * 
-  * - HISTORY
-  * 2021-07-14 : 초기 개발
-  * 2021-07-16 : 파일 인코딩 수정 및 주석 처리
-  * 2021-07-22 : TTS 기능 추가
-  * 2021-07-27 : 주석 처리 수정
-  * 2021-07-29 : 버그 수정
-  *
-  * <Variable>
-  * mg_targetPosition : inspector창에서 walkPos 오브젝트로 지정해서 walkPos 위치로 거인 이동시킴
-  * mg_Jack : 잭
-  * mb_checkPlayOnce : 스크립트 음성이 한번만 실행되게 하기 위한 체크하는 변수
-  * mb_JackFlag : Update 함수 안에서 Jack 한번만 찾도록 하는 변수
-  * sc : 동화 스크립트 나타내기 위한 오브젝트
-  * vm : 음성 TTS를 처리하는 오브젝트 연결
-  *
-  * <Function>
-  * MoveTowards() : 등속 이동, 매개변수로 {현재위치, 목표위치, 속도}를 입력           
-  */
+   * - Name: Movement_Giant.cs
+   * - Content: Jack and the Beanstalk Episode 8 - A class where a giant moves to chase after Jack/
+   *
+   * - HISTORY
+   * 2021-07-14: Initial development
+   * 2021-07-16: Fixed file encoding and commented out
+   * 2021-07-22: TTS function added
+   * 2021-07-27: Fix comment processing
+   * 2021-07-29: Bug fix
+   *
+   * <Variable>
+   * mg_targetPosition: Specifies the walkPos object in the inspector window and moves the giant to the walkPos position.
+   * mg_Jack: Jack
+   * mb_checkPlayOnce: Variable to check to ensure that the script voice is executed only once
+   * mb_JackFlag: Variable that allows Jack to be found only once in the Update function
+   * sc: Object to represent fairy tale script
+   * vm: Object connection that handles voice TTS
+   *
+   * <Function>
+   * MoveTowards(): Uniform speed movement, input {current position, target position, speed} as parameters
+   */
 
-using System.Collections;
+using System. Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//잭 뒤를 쫓도록 거인이 움직이는 클래스
+//Class that moves the giant to chase after Jack
 public class Movement_Giant : MonoBehaviour{
-  public GameObject mg_targetPosition;
-  GameObject mg_Jack;
-  bool mb_checkPlayOnce = false;
-  public ScriptControl sc;
-  VoiceManager vm;
-  bool mb_JackFlag = false;
+   public GameObject mg_targetPosition;
+   GameObject mg_Jack;
+   bool mb_checkPlayOnce = false;
+   public ScriptControl sc;
+   VoiceManager vm;
+   bool mb_JackFlag = false;
 
-  //초기설정
-  void Start(){
-    sc = ScriptControl.GetInstance();
-    this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
-    mg_Jack = GameObject.Find("Jack");
-  }
+   //Initial settings
+   void Start(){
+     sc = ScriptControl.GetInstance();
+     this.vm = GameObject.Find("VoiceManager").GetComponent<VoiceManager>();
+     mg_Jack = GameObject.Find("Jack");
+   }
   
-  void Update(){
-    if(vm.mb_checkSceneReady){                                                                                             //tts 준비 작업이 다 끝났다면 
-      transform.position = Vector3.MoveTowards(gameObject.transform.position, mg_targetPosition.transform.position, 0.1f); //거인 이동 
-      if(!mb_checkPlayOnce){                                                                                               //스크립트 음성이 한번도 나온적이 없다면
-        vm.playVoice(0);                                                                                                   //스크립트 음성 재생
-        mb_checkPlayOnce = true;                                                                                           //스크립트 음성 재생 체크
-      }
-      else if (!vm.isPlaying() && mb_JackFlag == false){
-        mg_Jack.GetComponent<Drag_Jack>().ChangeDragFlagTrue();
-        mb_JackFlag = true;
-      }
-    }
-  }
+   void Update(){
+     if(vm.mb_checkSceneReady){ //If tts preparation work is completed
+       transform.position = Vector3.MoveTowards(gameObject.transform.position, mg_targetPosition.transform.position, 0.1f); //giant movement
+       if(!mb_checkPlayOnce){ //If the script voice has never been played
+         vm.playVoice(0); //Play script voice
+         mb_checkPlayOnce = true; //Check script voice playback
+       }
+       else if (!vm.isPlaying() && mb_JackFlag == false){
+         mg_Jack.GetComponent<Drag_Jack>().ChangeDragFlagTrue();
+         mb_JackFlag = true;
+       }
+     }
+   }
 }
