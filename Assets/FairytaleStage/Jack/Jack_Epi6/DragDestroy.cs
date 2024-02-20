81c35b5c042989eb90bea4667daf1d370c6ff054
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:37:40 -0500

    Update DragDestroy.cs

--- Assets/FairytaleStage/Jack/Jack_Epi6/DragDestroy.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi6/DragDestroy.cs
@@ -1,36 +1,35 @@
-﻿/*
- * - Name : DragDestroy.cs
- * - Writer : 최대준
- * - Content : 잭과콩나무 에피소드1 - 
- * - History -
- * 2021-07-21 : 작성
- * 2021-07-27 : 피드백에 의한 주석 변경.
- *
- * - DragDestroy Member variables
- *
- * null
- *
- * - DragDestroy Member functions
- *
- * OnMouseDrag() : 드래그 시에 잭에게 띄워놓았던 말풍선을 없애는 함수이다.
- * SceneManager.LoadScene() : 다음 Scene으로 이동 하는 함수
- * 
- */
+/*
+  * - Name: DragDestroy.cs
+  * - Content: Jack and the Beanstalk Episode 1 -
+  * - History -
+  * 2021-07-21: Written
+  * 2021-07-27: Annotation changed based on feedback.
+  *
+  * - DragDestroy Member variables
+  *
+  *null
+  *
+  * - DragDestroy Member functions
+  *
+  * OnMouseDrag(): This is a function that removes the speech bubble displayed on Jack when dragging.
+  * SceneManager.LoadScene(): Function to move to the next scene
+  *
+  */
  
-using System.Collections;
+using System. Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 잭의 말풍선이 드래그하여 위치가 변했을 때에도 띄워져 있는 것이 부자연스러워 추가한 스크립트이다. 잭이 드래그되면 말풍선이 사라지도록 지정하였다.
+// This is a script added because it was unnatural for Jack's speech bubble to remain floating even when its position was changed by dragging. When the jack is dragged, the speech bubble is set to disappear.
 public class DragDestroy : MonoBehaviour {
-    // 드래그시에 말풍선을 사라지도록 하였다.
-    private void OnMouseDrag()
-    {
-        Destroy(GameObject.Find("speechBubble"));
-        Destroy(GameObject.Find("ScriptCanvas"));
-        Vector2 v2_checkMousePos = new Vector2(Input.mousePosition.x,
-        Input.mousePosition.y);
-        Vector2 v2_checkworldObjPos = Camera.main.ScreenToWorldPoint(v2_checkMousePos);
-        this.transform.position = v2_checkworldObjPos;
-    }
+     // The speech bubble disappears when dragged.
+     private void OnMouseDrag()
+     {
+         Destroy(GameObject.Find("speechBubble"));
+         Destroy(GameObject.Find("ScriptCanvas"));
+         Vector2 v2_checkMousePos = new Vector2(Input.mousePosition.x,
+         Input.mousePosition.y);
+         Vector2 v2_checkworldObjPos = Camera.main.ScreenToWorldPoint(v2_checkMousePos);
+         this.transform.position = v2_checkworldObjPos;
+     }
 }
