5d3cb7bace251d3f61861d62cbff07aa26de48b5
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:27:10 -0500

    Update ChangeScene.cs

--- Assets/FairytaleStage/Jack/Jack_Epi15/Scripts/ChangeScene.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi15/Scripts/ChangeScene.cs
@@ -1,35 +1,34 @@
 /*
- * - Name : ChangeScene.cs
- * - Writer : 이윤교
- * - Content : 잭과콩나무 에피소드15 - 잭과콩나무 동화 다시 듣기 버튼과 게임선택 씬으로 이동하는 버튼 스크립트
- * 
- * - HISTORY
- * 2021-07-20 : 초기 개발
- * 2021-07-27 : 주석 처리 수정
- *
- * <Function>
- * ReStart() : Jack_Epi1 씬 로드 함수 (동화 다시 재생 버튼)
- * Select() : select_stage 씬 로드 함수 
- */
+  * - Name: ChangeScene.cs
+  * - Content: Jack and the Beanstalk Episode 15 - Jack and the Beanstalk fairy tale replay button and button script to move to the game selection scene
+  *
+  * - HISTORY
+  * 2021-07-20: Initial development
+  * 2021-07-27: Fix comment processing
+  *
+  * <Function>
+  * ReStart(): Jack_Epi1 scene load function (story replay button)
+  * Select(): select_stage scene load function
+  */
 
-using System.Collections;
+using System. Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//잭과콩나무 동화 다시 듣기 버튼과 게임선택 씬으로 이동하는 버튼 스크립트
-public class ChangeScene : MonoBehaviour{
-    //Jack_Epi1 씬 로드 함수 (동화 다시 재생 버튼)
-    public void ReStart(){
-        SceneManager.LoadScene("Jack_Epi1");
-    }
+//Button script to replay the Jack and the Beanstalk fairy tale and move to the game selection scene
+public class ChangeScene: MonoBehaviour{
+     //Jack_Epi1 scene load function (story replay button)
+     public void ReStart(){
+         SceneManager.LoadScene("Jack_Epi1");
+     }
 
-    //select_stage 씬 로드 함수 
-    public void Select(){
-        SceneManager.LoadScene("select_stage_scene");
-        var obj = GameObject.Find("BGMmanager"); 
-        if(obj != null) { //BGM 끄기
-            Destroy(obj);
-        }
-    }
+     //select_stage scene load function
+     public void Select(){
+         SceneManager.LoadScene("select_stage_scene");
+         var obj = GameObject.Find("BGMmanager");
+         if(obj != null) { //Turn off BGM
+             Destroy(obj);
+         }
+     }
 }
