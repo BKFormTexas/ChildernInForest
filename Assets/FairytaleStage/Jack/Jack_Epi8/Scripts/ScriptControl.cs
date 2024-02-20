bde29df55bec635190636119e9865ca0ec60d67e
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:36:58 -0500

    Update ScriptControl.cs

--- Assets/FairytaleStage/Jack/Jack_Epi8/Scripts/ScriptControl.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi8/Scripts/ScriptControl.cs
@@ -1,69 +1,68 @@
 /*
-  * - Name : ScriptControl.cs
-  * - Writer : 이윤교
-  * - Content : 잭과콩나무 에피소드14 - ScriptControl
-  * 
-  * - HISTORY
-  * 2021-07-14 : 초기 개발
-  * 2021-07-16 : 파일 인코딩 수정 및 주석 처리
-  * 2021-07-27 : 주석 처리 수정
-  *
-  * <Variable>
-  * mg_targetPosition : inspector창에서 walkPos 오브젝트로 지정해서 walkPos 위치로 거인 이동시킴
-  * mb_checkPlayOnce : 스크립트 음성이 한번만 실행되게 하기 위한 체크하는 변수
-  * sc : 동화 스크립트 나타내기 위한 오브젝트
-  * vm : 음성 TTS를 처리하는 오브젝트 연결
-  *
-  * <Variable>
-  * instance : ScriptControl
-  * mg_setGameObject : Jack_Script 이름의 게임오브젝트를 찾아 저장
-  * mt_setText : mg_setGameObject오브젝트의 Text 컴포넌트를 담음.
-  * mn_checkCurrentScr : mt_setText에 나타낼 스크립트의 인덱스
-  * ms_setScriptText : 스크립트 갯수 만큼 스크립트 내용을 담는 리스트 
-  * mn_setScriptNum : 스크립트 초기 갯수 5개
-  *
-  * <Function>
-  * GetInstance() : instance 존재여부 확인 후 없으면 생성
-  * setNextScript() : 스크립트의 다음 문장을 보여줌.
-  */
+   * - Name: ScriptControl.cs
+   * - Content: Jack and the Beanstalk Episode 14 - ScriptControl
+   *
+   * - HISTORY
+   * 2021-07-14: Initial development
+   * 2021-07-16: Fixed file encoding and commented out
+   * 2021-07-27: Fix comment processing
+   *
+   * <Variable>
+   * mg_targetPosition: Specifies the walkPos object in the inspector window and moves the giant to the walkPos position.
+   * mb_checkPlayOnce: Variable to check to ensure that the script voice is executed only once
+   * sc: Object to represent fairy tale script
+   * vm: Object connection that handles voice TTS
+   *
+   * <Variable>
+   * instance: ScriptControl
+   * mg_setGameObject: Find and save the game object named Jack_Script
+   * mt_setText: Contains the Text component of the mg_setGameObject object.
+   * mn_checkCurrentScr: Index of script to display in mt_setText
+   * ms_setScriptText: List containing script content equal to the number of scripts
+   * mn_setScriptNum: Initial number of scripts: 5
+   *
+   * <Function>
+   * GetInstance(): Check whether instance exists and create it if it does not exist
+   * setNextScript(): Shows the next sentence of the script.
+   */
 
-using System.Collections;
+using System. Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//  Jack_Script 창에 스크립트를 보여줄 예정
-public class ScriptControl : MonoBehaviour{
-    /*싱글톤 구현*/
-    private static ScriptControl instance;     //여러 곳에서 동일한 인스턴스에 대해 공유할 수 있도록 static으로 선언
-    private GameObject mg_setGameObject;
-    private Text mt_setText;
-    private int mn_checkCurrentScr = 0;
-    public string[] ms_setScriptText = new string[mn_setScriptNum];
-    public const int mn_setScriptNum = 5;
+// Will display the script in the Jack_Script window
+public class ScriptControl: MonoBehaviour{
+     /*Singleton implementation*/
+     private static ScriptControl instance; //Declared as static so that the same instance can be shared in multiple places
+     private GameObject mg_setGameObject;
+     private Text mt_setText;
+     private int mn_checkCurrentScr = 0;
+     public string[] ms_setScriptText = new string[mn_setScriptNum];
+     public const int mn_setScriptNum = 5;
     
-    //초기설정
-    void Start(){
-        mg_setGameObject = GameObject.Find("Jack_Script"); 
-        mt_setText = mg_setGameObject.GetComponent<Text>(); 
-        mt_setText.text = ms_setScriptText[mn_checkCurrentScr];
-    }
+     //Initial settings
+     void Start(){
+         mg_setGameObject = GameObject.Find("Jack_Script");
+         mt_setText = mg_setGameObject.GetComponent<Text>();
+         mt_setText.text = ms_setScriptText[mn_checkCurrentScr];
+     }
 
-    //instance 존재여부 확인 후 없으면 생성하는 함수
-    public static ScriptControl GetInstance(){
-        if (!instance){                                                                     // 만약 instance가 존재하지 않을 경우
-            instance = GameObject.FindObjectOfType(typeof(ScriptControl)) as ScriptControl; // ScriptControl 클래스의 instance를 찾는다
-            if (!instance){                                                                 // 찾아봐도 존재하지 않을 경우 새로 만든다 
-                GameObject obj = new GameObject("ScriptControl");                           // 이름인 ScriptControl인 오브젝트 새로 생성
-                instance = obj.AddComponent(typeof(ScriptControl)) as ScriptControl;        // obj에 ScriptControl 타입의 컴포넌트를 추가
-            }
-        }
-        return instance;                                                                    // instance를 반환한다.
-    }
+     //Function to check whether the instance exists and then create it if it does not exist
+     public static ScriptControl GetInstance(){
+         if (!instance){ // if instance does not exist
+             instance = GameObject.FindObjectOfType(typeof(ScriptControl)) as ScriptControl; // Find an instance of the ScriptControl class
+             if (!instance){ // If it doesn't exist even after looking for it, create a new one
+                 GameObject obj = new GameObject("ScriptControl"); // Create a new object with the name ScriptControl
+                 instance = obj.AddComponent(typeof(ScriptControl)) as ScriptControl; // Add ScriptControl type component to obj
+             }
+         }
+         return instance; // Returns instance.
+     }
 
-    //스크립트의 다음 문장을 보여주는 함수
-    public void setNextScript(){ 
-        mn_checkCurrentScr++;                                                               //인덱스를 다음 스크립트로 이동
-        mt_setText.text = ms_setScriptText[mn_checkCurrentScr];                             //다음 스크립트를 보여줌
-    }
+     //Function to display the next statement of the script
+     public void setNextScript(){
+         mn_checkCurrentScr++; //Move index to next script
+         mt_setText.text = ms_setScriptText[mn_checkCurrentScr]; //Show the next script
+     }
 }
