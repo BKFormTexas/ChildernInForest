c61ad1e8bb25032f9b5f4d3eb229d9612f3c0f3a
Aidan(Byungkwon) Lee <88296556+BKFormTexas@users.noreply.github.com>
2024-02-20 10:41:20 -0500

    Update Jack4_Window.cs

--- Assets/FairytaleStage/Jack/Jack_Epi4/Scripts/Jack4_Window.cs
+++ Assets/FairytaleStage/Jack/Jack_Epi4/Scripts/Jack4_Window.cs
@@ -1,60 +1,60 @@
-﻿/*
- * - Name : Jack4_Mother.cs
- * - Writer : 김명현
- * 
- * - Content :
- * 잭과콩나무 에피소드4 - 어머니 오브젝트 스크립트
- * 어머니와 콩의 객체 충돌처리를 위한 스크립트
- * 
- *  * - Update Log -
- * 2021-07-13 : 제작 완료
- * 2021-07-23 : 주석 변경
- *          
- * - Variable
- * mg_EventManager                          감독 오브젝트 연결을 위한 변수
- * mg_Bean                                  콩 오브젝트 연결을 위한 변수
- * 
- * - Function
- * OnTriggerEnter2D(Collider2D cCollidObj)  충돌감지 함수
- * 
- */
+/*
+  * - Name: Jack4_Mother.cs
+  * - Writer: Myeonghyun Kim
+  *
+  * - Content:
+  * Jack and the Beanstalk Episode 4 - Mother Object Script
+  * Script for object conflict handling between mother and bean
+  *
+  * * - Update Log -
+  * 2021-07-13: Production completed
+  * 2021-07-23: Comment change
+  *
+  * - Variable
+  * Variable for connecting mg_EventManager director object
+  * mg_Bean Variable for bean object connection
+  *
+  * - Function
+  * OnTriggerEnter2D(Collider2D cCollidObj) collision detection function
+  *
+  */
 
-using System.Collections;
+using System. Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Jack4_Window : MonoBehaviour
 {
-    GameObject mg_EventManager;
-    GameObject mg_Bean;
+     GameObject mg_EventManager;
+     GameObject mg_Bean;
 
-    bool CheckFlag;
+     bool CheckFlag;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        this.mg_EventManager = GameObject.Find("GameDirector");
-        this.mg_Bean = GameObject.Find("Bean");
+     // Start is called before the first frame update
+     void Start()
+     {
+         this.mg_EventManager = GameObject.Find("GameDirector");
+         this.mg_Bean = GameObject.Find("Bean");
 
-    }
+     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CheckFlag = this.mg_EventManager.GetComponent<Jack4_EventController>().b_CheckBeanToMother();
-    }
+     // Update is called once per frame
+     void Update()
+     {
+         CheckFlag = this.mg_EventManager.GetComponent<Jack4_EventController>().b_CheckBeanToMother();
+     }
 
-    /// <summary>
-    /// 충돌이 일어나고 있는동안 작동되는 함수
-    /// </summary>
-    /// <param name="cCollidObj">충돌된 오브젝트</param>
-    void OnTriggerEnter2D(Collider2D cCollidObj)
-    {
-        Debug.Log("충돌 감지");
-        if (cCollidObj.tag == "Bean" && CheckFlag == true)
-        {
-            Destroy(cCollidObj.gameObject);
-            this.mg_EventManager.GetComponent<Jack4_EventController>().v_BeanToWindow();
-        }
-    }
+     /// <summary>
+     /// Function to run while a collision is occurring
+     /// </summary>
+     /// <param name="cCollidObj">Collided object</param>
+     void OnTriggerEnter2D(Collider2D cCollidObj)
+     {
+         Debug.Log("Collision Detected");
+         if (cCollidObj.tag == "Bean" && CheckFlag == true)
+         {
+             Destroy(cCollidObj.gameObject);
+             this.mg_EventManager.GetComponent<Jack4_EventController>().v_BeanToWindow();
+         }
+     }
 }
